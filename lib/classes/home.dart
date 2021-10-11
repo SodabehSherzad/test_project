fb86b7e665078cf87f1acf2728da5d2b1793a2d1
Sodabeh Sherzad <42426114+SodabehSherzad@users.noreply.github.com>
2021-10-12 00:58:45 +0430

    change query
    When change language, query does not update

--- lib/classes/home.dart
+++ lib/classes/home.dart
@@ -1,5 +1,8 @@
-class Home {
+import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:test_project/localization/language_constants.dart';
 
+class Home {
   //these variables are for home table
   int _id;
   String _body;
@@ -15,6 +18,25 @@ class Home {
     this._id = map['id'];
     this._body = map['body'];
   }
+}
 
+class HomeProvider with ChangeNotifier {
+  // NOTE: I assume that you have a list of home data in the database.
+  List<Home> _homes = [];
+  List<Home> get homes {
+    return [..._homes];
+  }
 
+  // This method does not update data when change languages***
+  Future<void> getHomeData(Database database, context) async {
+    String query =
+    (getTranslated(context, "language") == "fa")?
+    "SELECT * FROM home_fa"
+    : "SELECT * FROM home_en";
+
+    var res = await database.rawQuery(query);
+    Home model = res.isNotEmpty ? Home.fromMapObject(res.first) : null;
+    _homes.add(model);
+    notifyListeners();
+  }
 }
